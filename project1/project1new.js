53a589725d3235c37b0ec57c350116aa4e4385c8
rishikeshhh123 <sv2113340@gmail.com>
2023-12-31 21:14:34 +0530

    project1

---
+++ project1/project1new.js
@@ -0,0 +1,33 @@
+//event.preventDefault()
+
+            let data = "\r amount " + amount.value + " \r\n " + "type1 " + type1.value + " \r\n " + "type2 " + type2.value  ;
+            console.log(data); 
+            localStorage.setItem('hello', document.getElementById('hello').value);
+            const name = localStorage.getItem('hello');
+
+            //
+            document.getElementById('whole').addEventListener('submit', function(event) {
+                event.preventDefault();
+                const amount = document.getElementById("hello").value;
+            const type1 = document.getElementById("choose1").value;
+            const type2 = document.getElementById("choose2").value;
+            let t=amount.getElementById("hello");
+            console.log(t.value);
+               
+            
+                const formData = {
+                    amount: amount,
+                   type1: type1,
+                   type2: type2
+                };
+            
+                saveFormData(formData);
+            });
+            
+            function saveFormData(formData) {
+                const storedFormData = JSON.parse(localStorage.getItem('formData')) || [];
+            
+                storedFormData.push(formData);
+            
+                localStorage.setItem('formData', JSON.stringify(storedFormData));
+            }
