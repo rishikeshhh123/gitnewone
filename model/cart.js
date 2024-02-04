03b75ce42275b36cf71a30bfba2a65cd6bd3a3b7
rishikesh <sv2113440@gmail.com>
2024-02-04 13:13:22 +0530

    adding cart

---
+++ model/cart.js
@@ -0,0 +1,31 @@
+const fs=require('fs')
+const path=require('path');
+const p=path.join(path.dirname(require.mainModule.filename),data,cart.json)
+
+module.exports=class cart{
+static addProduct(id,productPrice){
+    fs.File(p,(err,fileContent)=>{
+    let cart={products:[],totalPrice:0}
+    if(!err){
+        cart=JSON.parse(fileContent)
+    }
+    const existingCart=cart.products(prod=>prod.id===id);
+    const existingProduct=cart.products[existingCart]
+    let updatedProduct;
+    if(existingCart){
+        updatedProduct={...existingProduct}
+        updatedProduct.qty=updatedProduct.qty+1;
+        cart.products=[...cart.products];
+        cart.products[existingCart]=  updatedProduct
+    }
+    else{
+        updatedProduct={id:id,qty:1}
+        cart.products=[...cart.products,updatedProduct]
+    }
+    cart.totalPrice+=+productPrice;
+    fs.writeFile(p,JSON.stringify(cart),err=>{
+        console.log(err)
+    })
+    })
+}
+}
