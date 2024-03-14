f5dcf449106d1e995380906aa9604e33286a002d
rishikesh <sv2113440@gmail.com>
2024-03-14 18:20:46 +0530

    private keys moved to env

--- expense_tracker/backend/util/database.js
+++ expense_tracker/backend/util/database.js
@@ -1,8 +1,10 @@
 const  Sequelize  = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const sequelize = new Sequelize('node-complete', 'root', 'Sachin@123', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME,process.env.DB_PASSWORD, {
   dialect: 'mysql',
-  host: 'localhost'
+  host: process.env.DB_HOST
 });
 
 sequelize.authenticate()
